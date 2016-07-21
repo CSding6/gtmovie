5bc24ad95943f4010e78d3da746593c91dd428dc
Li <csding6@gmail.com>
2016-07-21 19:01:35 -0400

    update

--- insert_select.sql
+++ insert_select.sql
@@ -13,3 +13,24 @@ VALUES
 (
 
 );
+
+-- view review
+select review_ID, title, comment, rating, username from Review;
+
+-- check movie status if the user already watched the movie
+   select status
+     from Order_info as o
+left join Movie      as m
+       on o.title = m.title;
+    where o.username = %s; -- input username
+
+
+-- serach all theater
+select * from Theater;
+
+-- serach theater from Preferred list
+    select * 
+      from Prefers as p
+ left join Theater as t
+        on p.theater_ID = t.theater_ID
+     where p.username = %s -- input username
