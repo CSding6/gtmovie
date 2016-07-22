a3f8104432b7a4aed2fd2502375bd1ca4fbbd1ee
Li <csding6@gmail.com>
2016-07-21 20:53:39 -0400

    update

--- insert_select.sql
+++ insert_select.sql
@@ -90,3 +90,74 @@ left join Movie    as m
       from System_info;
 
 
+-- for payment
+-- use a saved card
+    select *
+      from Payment_info as p 
+     where p.username = %s; -- input username
+
+
+-- save a new card
+-- need to check data
+INSERT INTO Payment_info
+(
+    card_num,
+    name_on_card,
+    cvv,
+    saved,
+    expiration_date
+    username
+)
+VALUES 
+(
+    -- expiration_date needs to be checked
+);
+
+
+-- generated a new order
+INSERT INTO Order_info
+(
+    status,
+    time,
+    date,
+    num_of_child_ticket,
+    num_of_adult_ticket,
+    num_of_senior_ticket,   
+    num_of_total_ticket,
+    username
+    card_num
+    theater_ID
+)
+VALUES
+(
+
+);
+
+-- get order history
+-- order_id, movie, stauts, total
+with tt_sys as (
+    select child_discount,
+           senior_discount
+      from System_info
+)
+   select order_id,
+          title,
+          status,
+          (num_of_child_ticket * % * tt_sys.child_discount 
+            + num_of_senior_ticket * % * tt_sys.senior_discount
+            + num_of_adult_ticket * %) as total
+     from Order_info
+ order by order_id;
+
+ -- get order history detail
+
+   select s.show_time
+     from Order_info as o 
+left join Theater    as t
+       on o.theater_ID = o.theater_ID
+left join Movie      as m
+       on m.title = o.title
+left join Show_time  as s
+       on s.theater_ID = o.theater_ID
+      and m.title = o.title
+    where o.order_id = %s;
